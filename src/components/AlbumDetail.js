624dbb978df9b3d777ba533078b8e5fd94b28662
Jad Carson <Jad.Carson@gmail.com>
2016-09-30 16:23:56 -0500

    added album detail section with images and flexbox

--- src/components/AlbumDetail.js
+++ src/components/AlbumDetail.js
@@ -1,16 +1,51 @@
 import React from 'react';
-import { Text } from 'react-native';
+import { Text, View, Image } from 'react-native';
 import Card from './Card';
 import CardSection from './CardSection';
 
 const AlbumDetail = (props) => {
+  const { thumbnailStyle, headerContentStyle, thumbnailContainerStyle, headerTextStyle, imageStyle } = styles;
   return (
     <Card> 
       <CardSection>
-        <Text>{props.album.title}</Text>
+      <View style={thumbnailContainerStyle}>
+        <Image source={{ uri: props.album.thumbnail_image }} style={thumbnailStyle} />
+      </View>
+      <View style={headerContentStyle}>
+        <Text style={headerTextStyle}>{props.album.title}</Text>
+        <Text>{props.album.artist}</Text>
+      </View>
+      </CardSection>
+      <CardSection>
+      <Image source={{ uri: props.album.image }} style={imageStyle} />
       </CardSection>
     </Card>
     );
+};
+
+const styles = {
+  headerContentStyle: {
+    flexDirection: 'column',
+    justifyContent: 'space-around'
+  },
+  headerTextStyle: {
+    fontSize: 18
+  },
+  thumbnailStyle: {
+    height: 50,
+    width: 50
+  },
+  thumbnailContainerStyle: {
+    justifyContent: 'center',
+    alignItems: 'center',
+    marginLeft: 10,
+    marginRight: 10
+  },
+  imageStyle: {
+    height: 300,
+    flex: 1,
+    width: null
+  }
 };
 
 export default AlbumDetail;
